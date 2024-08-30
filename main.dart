84253898234202b434d340918308e2f2b0114a23
ShifaKayana <141887211+ShifaKayana@users.noreply.github.com>
2024-08-30 12:41:28 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,115 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SimpleCalculator(),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    home: MasukkanNilai(),
+  ));
 }
 
-class SimpleCalculator extends StatefulWidget {
-  @override
-  _SimpleCalculatorState createState() => _SimpleCalculatorState();
-}
+class MasukkanNilai extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
 
-class _SimpleCalculatorState extends State<SimpleCalculator> {
-  String display = '0';
-  double num1 = 0;
-  String operator = '';
-  Color backgroundColor = const Color.fromARGB(255, 46, 35, 35);
-
-  void _onPressed(String text) {
-    setState(() {
-      if (text == 'C') {
-        display = '0';
-        operator = '';
-      } else if (text == '+' || text == '-' || text == '*' || text == '/') {
-        num1 = double.parse(display);
-        operator = text;
-        display = '0';
-      } else if (text == '=') {
-        double num2 = double.parse(display);
-        if (operator == '+') {
-          display = (num1 + num2).toString();
-        } else if (operator == '-') {
-          display = (num1 - num2).toString();
-        } else if (operator == '*') {
-          display = (num1 * num2).toString();
-        } else if (operator == '/') {
-          display = (num1 / num2).toString();
-        }
-        operator = '';
-      } else {
-        if (display == '0') {
-          display = text;
-        } else {
-          display += text;
-        }
-      }
-    });
-  }
-
-  Widget _buildButton(String text) {
-    return Expanded(
-      child: ElevatedButton(
-        onPressed: () => _onPressed(text),
-        child: Text(text, style: TextStyle(fontSize: 20)),
-      ),
-    );
+  String classifyGrade(int score) {
+    if (score >= 85) return 'A';
+    if (score >= 70) return 'B';
+    if (score >= 55) return 'C';
+    return 'D';
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculator'),
+        title: Text('Pengelompokan Nilai'),
       ),
-      body: Container(
-        color: backgroundColor,
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(24.0),
-                alignment: Alignment.centerRight,
-                child: Text(display, style: TextStyle(fontSize: 48)),
+          children: [
+            TextField(
+              controller: _controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Masukkan nilai',
               ),
             ),
-            Row(
-              children: <Widget>[
-                _buildButton('7'),
-                _buildButton('8'),
-                _buildButton('9'),
-                _buildButton('/'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _buildButton('4'),
-                _buildButton('5'),
-                _buildButton('6'),
-                _buildButton('*'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _buildButton('1'),
-                _buildButton('2'),
-                _buildButton('3'),
-                _buildButton('-'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                _buildButton('0'),
-                _buildButton('C'),
-                _buildButton('='),
-                _buildButton('+'),
-              ],
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                final int? score = int.tryParse(_controller.text);
+                final String result = (score != null)
+                    ? 'Kategori: ${classifyGrade(score)}'
+                    : 'Masukkan nilai yang valid';
+
+                showDialog(
+                  context: context,
+                  builder: (_) => AlertDialog(
+                    content: Text(result),
+                  ),
+                );
+              },
+              child: Text('Hitung Nilai'),
             ),
           ],
         ),
